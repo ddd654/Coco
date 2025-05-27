f76d6e9be13e85ea45842e552496abd40233f3ed
bound654 <bound654@naver.com>
2025-05-27 16:56:43 +0900

    컴포넌트2

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import './App.css';
 import React, { use, useEffect, useState } from 'react';
 import axios from 'axios';
 
-// 갑자기 느낀건데 납득이 안돼
-// 직접 만들면서 느껴야겠어
-
 //function = 컴포넌트 자체, 부품
 //props = 속성, 연결
 
 function Header(props) { //Header 컴포넌트
   console.log("헤더의 프롭스와 타이틀", props, '', props.title);
-
+  console.log();
   return (
     <header>
       <h1>
-        <a href='/' onClick={(event) => {
+        <a className='changeTitle' href='/' onClick={(event) => {
           event.preventDefault();
           props.onChangeMode();
         }}>
-          Web
+          {props.title}
         </a>
       </h1>
     </header>
@@ -27,20 +24,21 @@ function Header(props) { //Header 컴포넌트
 }
 
 function Nav(props) { //Nav 컴포넌트
-  const list = [
-  ]
-  console.log('프롭스가 뭐야', props);
+  const list = []
 
   for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i]; //topics의 배열 및 객체
+    let t = props.topics[i];
     list.push(
       <li key={t.id}>
-        <a href={'/read/' + t.id}>
+        <a id={t.id} href={'/Read/' + t.id} onClick={event => {
+          event.preventDefault();
+          props.onChangeMode(Number(event.target.id));
+        }}>
           {t.title}
         </a>
-      </li>
-    )
+      </li>)
   }
+
   return (
     <nav>
       <ol>
@@ -60,8 +58,9 @@ function Article(props) { //Article 컴포넌트
 }
 
 function App() { //메인 App 컴포넌트
-  const mode = 'Welcome';
-  const topics = [ // 아 이게 data (임시)
+  const [mode, setMode] = useState('Welcome');
+  const [id, setId] = useState(null);
+  const topics = [ // 아 data (임시)
     { id: 1, title: 'html', body: 'html\'s body value' },
     { id: 2, title: 'css', body: 'css\'s body value' },
     { id: 3, title: 'java title', body: 'java\'s \\ body value' }
@@ -70,17 +69,30 @@ function App() { //메인 App 컴포넌트
 
   let content = null;
   if (mode === 'Welcome') {
-    content = <Article title='theory - updated version' body='life is boring and bored'></Article>
+    content = <Article title='apple' body='맛있는 사과'></Article>;
   } else if (mode === 'Read') {
-    content = <Article title='Read' body='Read this body'></Article>
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      console.log('아이디~' + topics[i].id, id);
+      if (topics[i].id == id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>;
   }
 
   return (
     <div>
       <Header title='React' onChangeMode={function () {
-        alert('인생이란 어떻게다');
+        alert('인생이란 어떻게');
+        setMode('Welcome');
       }}></Header>
-      <Nav topics={topics}></Nav>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        alert('아이디 값: ' + _id);
+        setMode('Read');
+        setId(_id);
+      }}></Nav>
       {content}
     </div>
   );
