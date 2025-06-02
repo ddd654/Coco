174b3f710b87db18eeb855d9aeb3ffdef637d591
bound654 <bound654@naver.com>
2025-06-02 16:54:35 +0900

    Document objecet model?

---
+++ js/header.js
@@ -0,0 +1,21 @@
+
+//의미: 이걸 작성하면 헤더 메뉴가 만들어진다
+
+//만들것: menu를 클릭했을때 안쪽 span 태그들의 각도가 바뀌어야한다
+const menu = document.querySelector('.menu');
+const menu_span = document.querySelector('.menu span');
+
+
+
+const li = document.querySelectorAll('li');
+console.log('li All은?',li);
+
+const li_tag= document.getElementsByTagName('li');
+console.log(li_tag);
+
+
+
+
+
+
+
