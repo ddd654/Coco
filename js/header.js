34a4cce937bc967021f17fe43a3911f78fcfe690
bound654 <bound654@naver.com>
2025-06-10 16:15:05 +0900

    레이아웃1

--- js/header.js
+++ js/header.js
@@ -1,56 +1,103 @@
 
-//의미: 이걸 작성하면 헤더 메뉴가 만들어진다
-
 //만들것: menu를 클릭했을때 안쪽 span 태그들의 각도가 바뀌어야한다
 const menu = document.querySelector('.menu');
 const menuSpan = document.querySelector('.menu span');
 
+let menuState = false;
 menu.addEventListener('click', () => { //menu를 클릭하면 menu모양이 바뀌는 이벤트
   const firstSpan = menu.firstElementChild;
+  const midSpan = menu.children[1];
   const lastSpan = menu.lastElementChild;
 
-  const currentTransform = firstSpan.style.transform;
+  console.log(midSpan);
 
-  if (currentTransform == 'rotate(45deg) scale(1.1, 1)') {//클릭했을때 이미 45도면
-    firstSpan.style.transform = 'rotate(0deg) scale(1.0, 1)';
+  if (menuState == true) {//클릭했을때 이미 45도면, true이면 열린 상태
+    firstSpan.style.transform = 'translate(0px, 0px) rotate(0deg) ';
+    midSpan.style.opacity = 1;
+    lastSpan.style.transform = 'translate(0px, 0px) rotate(0deg) ';
+    menuState = false;
   } else {
-    firstSpan.style.transform = 'rotate(45deg) scale(1.1, 1)'
+    firstSpan.style.transform = 'translate(0px, 13.5px) rotate(45deg)';
+    midSpan.style.opacity = 0;
+    lastSpan.style.transform = 'translate(0px, -13.5px) rotate(-45deg)';
+    menuState = true;
   }
 })
 
+//-------------------------
+// Event 살펴보기
+// const firstInput =document.querySelector('.firstInput');
+// firstInput.addEventListener('keyup', (e)=>{
+//   console.log(e);
+// })
 
-// -- 
-// cloneNode, remove, 태그의 부모 형제 자식 선택 방법을 했음
+// firstInput.addEventListener('focus',()=>{
+//   firstInput.style.backgroundColor = 'rgba(255, 0, 0, 0.2)';
+// })
 
-// --
-const openedMenu = document.querySelector('.openedMenu');
-openedMenu.style.width = '200px';
-openedMenu.style.height = '';
-openedMenu.style.color = 'white';
-// openedMenu.style.backgroundColor = 'greenyellow';
-openedMenu.style['fontSize'] = '20px'; //다르게 사용하는 방법
-openedMenu.style.border = '10px solid black'
+// firstInput.addEventListener('blur', ()=>{
+//   firstInput.style.backgroundColor = null;
+// })
+
+// firstInput.addEventListener('mousemove', (e)=>{
+//   console.log(e);
+// })
+
+// const openedMenu = document.querySelector('.openedMenu');
+// const circle = document.querySelector('.circle');
+// const color = document.querySelector('#color');
+// const red = document.querySelector('#red');
+
+// openedMenu.addEventListener('mousemove', (e) => {
+//   circle.style.top = `${e.clientY}px`;
+//   circle.style.left = `${e.clientX}px`;
+// })
+
+// window.addEventListener('resize', (e) => {
+//   document.body.innerText = `현재 창 크기는 ${window.innerWidth} x ${window.innerHeight}`;
+// })
+
+// openedMenu.addEventListener('click', ()=>{
+//   console.log('1 첫번째 메뉴');
+// })
+
+// circle.addEventListener('click',()=>{
+//   console.log('2 circle');
+// })
+
+// color.addEventListener('click',()=>{
+//   console.log('2-1 color');
+// })
+
+// red.addEventListener('click',()=>{
+//   console.log('3 red');
+// })
+
+// //------
+
+// const div =document.querySelector('.div');
+// const ulList = document.querySelector('.ulList');
+// const colors = ulList.children;
+
+// console.log(colors);
+
+// function clickHandler(event){
+//   for(c of colors){
+//     c.classList.remove('on');
+//   }
+//   event.target.classList.add('on');
+//   console.log('on을 바꿨어')
+// }
+
+// document.querySelector('.red').addEventListener('click', clickHandler);
+// document.querySelector('.blue').addEventListener('click', clickHandler);
+// document.querySelector('.green').addEventListener('click', clickHandler);
+// document.querySelector('.pink').addEventListener('click', clickHandler);
+
+// document.querySelector('.ulList').addEventListener('click', clickHandler);
 
 
-console.log(openedMenu.className);
-openedMenu.className = 'bg-red';
-console.log(openedMenu.classList);
-openedMenu.classList.remove('bg-red');
 
-// setInterval(()=>{
-// openedMenu.classList.toggle('bg-red');
-// }, 1000)
-
-const color = document.querySelector('#color');
-
-color.onclick = function (e) {
-  console.log(e.target);
-  const target = e.target;
-  console.log(target.tagName);
-
-  if (target.tagName !== 'LI') { return } //LI 태그가 아니면 아무일도
-  target.classList.toggle('txt-pink');
-}
 
 
 
---
+++ js/study/js.js
@@ -0,0 +1,136 @@
+
+// alert('일방적인 통보 메시지');
+// prompt('예약일을 입력하세요', '2100-01-01');
+// confirm('할 마음이 생겼습니까?');
+
+
+// function sayHello(name = 'Chris'){
+//   // let newName = name || 'Chris';
+//   let msg = 'Hello';
+//   console.log(msg + ` `+ name);
+// }
+
+// function showError(){
+//   console.log('error 발생 로그');
+// }
+
+
+// sayHello();
+// showError();
+
+
+// function add(num1, num2){
+//   return num1 + num2;
+// }
+
+// const result = add(22, 44);
+// console.log(result);
+
+// function showError(){
+//   alert('에러 발생');
+// }
+
+// const result2 = showError();
+// console.log(result2);
+
+
+
+// let showError = () => {
+//   console.log('에러에요');
+// }
+// showError();
+
+
+// const add = (num1, num2) => {
+//   num1 + num2;
+// }
+
+// const coco = {
+//   name: 'coco',
+//   age: 33,
+// }
+
+
+// console.log(coco);
+// coco.hairColor = 'black';
+// coco['habby'] = 'cycle';
+
+// delete coco.age;
+
+// function makeObject(name, age){
+//   return{
+//     name,
+//     age,
+//     habby: 'cycle',
+//   }
+// }
+
+// const Chris= makeObject('Chris', 33);
+// console.log(Chris);
+// console.log('age' in Chris);
+// console.log('birthday' in Chris);
+
+// function isAdult(user){
+//   if(user.age < 22){
+//     return false;
+//   } else{
+//     return true;
+//   }
+// }
+
+// const Chris2 ={
+//   name: 'Chris',
+//   age: 22
+// }
+// const coco2 ={
+//   name:'coco',
+// }
+
+// console.log('chris2의 나이기준 ', isAdult(Chris2))
+
+
+// const BreakFast ={
+//   name:'',
+//   kcal:0,
+//   gram:0,
+// }
+// BreakFast.name = 'egg';
+// BreakFast.kcal = 222;
+// BreakFast.kcal = 10;
+// console.log(BreakFast.name, BreakFast.kcal, BreakFast.gram);
+
+
+
+// //Apple 객체
+// const Apple={
+//   name:'apple',
+//   color:'red',
+//   size:'big',
+// }
+// //x 키와 각 x의 값들 조회하기
+// for(x in Apple){
+//   console.log(x,Apple[x]);
+// }
+
+
+// let week = ['mon', 'tue', 'wed', 'fri', 'sat', 'sun'];
+
+// week[1] = '화';
+// week.push('thu');
+// week.unshift('sun');
+
+// console.log(week);
+
+
+// for(let i = 0; i< week.length; i++){
+//   console.log(week[i]);
+// }
+
+//변수, 자료형, 모달창 알럿 프롬 컨펌, 연산자, 조건문, 반복문, 함수, 객체, 배열...
+// 다음이 중급이랑 DOM 형식 둘 중 하나인데
+
+//--------------------
+
+
+
+
